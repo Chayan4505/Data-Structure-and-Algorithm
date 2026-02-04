9f0e568eab6dae2643a1888b294f72cc65f0101f
Chayan Mal <chayan2005mal@gmail.com>
2026-02-05 00:47:33 +0530

    Add files via upload

---
+++ DFS.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<list>
+using namespace std;
+
+class Graph{
+    int V;
+    list<int>* l;
+  public:
+    Graph(int V){
+        this->V = V;
+        l = new list<int> [V];
+
+    }  
+
+    void addEdge(int u, int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+
+    }
+
+
+    void DFS_helper( int u, vector<bool> &vis){
+        vis[u] = true;
+        cout<< u <<" ";
+
+        list<int> neighbours = l[u];
+        for(int v: neighbours){
+            if (!vis[v])
+            {
+                DFS_helper(v, vis);
+            }
+            
+        }
+    }
+
+    void DFS(){
+        vector<bool> vis(7, false);
+        DFS_helper(0, vis);
+    }
+
+};
+
+int main(){
+    Graph graph(7);
+
+    graph.addEdge(0,1);    
+    graph.addEdge(0,2);
+    graph.addEdge(1,3);
+    graph.addEdge(2,4);
+    graph.addEdge(3,4);
+    graph.addEdge(3,5);
+    graph.addEdge(4,5);
+    graph.addEdge(5,6);
+
+    graph.DFS();
+
+    return 0;
+}
